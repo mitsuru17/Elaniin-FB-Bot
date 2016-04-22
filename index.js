ee96f398416bf6c5abc2fba9c7584d4063a0b491
Adrián Gómez <g.adrian@elaniin.com>
2016-04-22 14:28:26 -0600

    SMS Completed

--- index.js
+++ index.js
@@ -134,9 +134,11 @@ function sendSMS(sender,number,messagex){
       url:     'https://api.inxights.co/general/sendsms/',
       body:    'country_code=503&to=' + number + '&message=' + messagex
     }, function(error, response, body){
-      sendTextMessage(sender, "Mensaje enviado con exito!");
-      sendTextMessage(sender, body);
-
+        if (body == text.indexOf('"code":"01"') > -1) {
+            sendTextMessage(sender, "Mensaje enviado con exito al " + number + "!");
+        }else {
+            sendTextMessage(sender, 'Lo siento, no pude enviar tu mensaje favor intenta de nuevo recuerda escribir: "enviar + numero + mensaje"');
+        }
     });
   
 }
