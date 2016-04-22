80cac617d4ca4c2e204b0f9288723224bf25c726
Adrián Gómez <g.adrian@elaniin.com>
2016-04-22 01:15:48 -0600

    First Tester Team

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ app.post('/webhook/', function (req, res) {
             else if (text.indexOf("chiste") > -1) {
                 sendTextMessage(sender, "¿Tienes wi-fi? Sí ¿Y cuál es la clave? Tener dinero y pagarlo.");
             }
-            else if (text.indexOf("contacto") > -1) {
+            else if (text.indexOf("contacto") > -1 || text.indexOf("contactar") > -1) {
                 sendTextMessage(sender, "Puedes escribirnos a hello@elaniin.com o llamarnos desde El Salvador al 2297-9723 o desde USA al (813)922-3440");
             }
             else if (text == "equipo") {
-                sendTextMessage(sender, "Nuestro equipo de trabajo es");
+                sendTeamMessage(sender);
             }
             else if (text.indexOf("cotiza") > -1) {
                 sendTextMessage(sender, "Para cotizar cualquier de nuestros servicios puedes ingresar al siguiente link: https://elaniin.com/cotiza-tu-proyecto/");
@@ -65,11 +65,11 @@ app.post('/webhook/', function (req, res) {
                 sendTextMessage(sender, "Para enviar un sms debes escribir: 'enviar + numero + mensaje'");
             }
             else if (text.indexOf("enviar") > -1) {
-                sendSMS("72600261","vamos hacerlo");
+                //sendSMS("72600261","vamos hacerlo");
                 sendTextMessage(sender, "Mensaje enviado con exito!");
             }
             else{
-                sendTextMessage(sender, "¿Qué te gustaria saber de nosotros? ¿Información de contacto, cotizar un proyecto, conocer a nuestro equipo, enviar un SMS o leer un chiste?");
+                sendTextMessage(sender, "¿Qué te gustaria saber de nosotros? ¿Información de contacto, cotizar un proyecto, conocer a nuestro equipo o leer un chiste?");
             }
             
 
@@ -91,17 +91,7 @@ app.listen(port, function () {
 //send SMS with Twillio
 function sendSMS(number,messagex){
 
-    
-    var url = 'https://api.inxights.co/general/sendsms/';
-    var headers = { 
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-        'Content-Type' : 'application/x-www-form-urlencoded' 
-    };
-    var form = { country_code: '503', to: number, message: messagex};
-
-    request.post({ url: url, form: form, headers: headers }, function (e, r, body) {
-        // your callback body
-    });    
+  
 }
 //send Message with Facebook Graph Facebook v2.6
 function sendTextMessage(sender, text) {
@@ -128,4 +118,53 @@ function sendTextMessage(sender, text) {
 
     });
 
+}
+
+function sendTeamMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
 }
